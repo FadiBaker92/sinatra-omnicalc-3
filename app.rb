843aebb50a5459ca9fa27554355ccecfb75a2335
FADI BAKER <133148112+FadiBaker92@users.noreply.github.com>
2024-03-25 21:15:46 +0000

    fetched lng and long. Gmaps done!

--- app.rb
+++ app.rb
@@ -18,6 +18,10 @@ get("/process_umbrella") do
   gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@user_location}&key=#{ENV["GMAPS_KEY"]}"
   raw_results = HTTP.get(gmaps_url).to_s
   @parsed_result = JSON.parse(raw_results)
+  @result_location = @parsed_result.dig("results", 0, "geometry", "location")
+  @latitude = @result_location.fetch("lat")
+  @longitude = @result_location.fetch("lng")
+  
 
   erb(:umbrella_results)
 end
