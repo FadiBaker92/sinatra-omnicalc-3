77f0601a7d4ce0c08413e454c495df66b579df76
FADI BAKER <133148112+FadiBaker92@users.noreply.github.com>
2024-03-25 21:22:17 +0000

    added a gsub to replace the spaces with a ' + ' sign

--- app.rb
+++ app.rb
@@ -15,7 +15,8 @@ end
 
 get("/process_umbrella") do
   @user_location = params.fetch("user_loc")
-  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@user_location}&key=#{ENV["GMAPS_KEY"]}"
+  user_location_gsub = @user_location.gsub(" ", "+")
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location_gsub}&key=#{ENV["GMAPS_KEY"]}"
   raw_results = HTTP.get(gmaps_url).to_s
   @parsed_result = JSON.parse(raw_results)
   @result_location = @parsed_result.dig("results", 0, "geometry", "location")
