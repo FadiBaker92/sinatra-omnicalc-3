ecb38fd15b261dc2307064af797897ac12cc5873
FADI BAKER <133148112+FadiBaker92@users.noreply.github.com>
2024-03-26 19:32:32 +0000

    pirate_weather done

--- app.rb
+++ app.rb
@@ -19,7 +19,26 @@ post("/process_umbrella") do
   @result_location = @parsed_result.dig("results", 0, "geometry", "location")
   @latitude = @result_location.fetch("lat")
   @longitude = @result_location.fetch("lng")
-    
+  pirate_weather_url = "https://api.pirateweather.net/forecast/#{ENV["PIRATE_WEATHER_KEY"]}/#{@latitude},#{@longitude}"
+  pirate_raw_result = HTTP.get(pirate_weather_url).to_s
+  pirate_parsed_result = JSON.parse(pirate_raw_result)
+  @pirate_temperature = pirate_parsed_result.dig("currently" , "temperature")
+  @pirate_summ = pirate_parsed_result.dig("currently" , "summary")
+
+  precip_prob_threshold = 0.10
+  @any_precipitation = false
+  hourly_hash = pirate_parsed_result.fetch("hourly")
+  hourly_data_array = hourly_hash.fetch("data")
+  next_twelve_hours = hourly_data_array[1..12]
+  
+  next_twelve_hours.each do |hour_hash| 
+    precip_prob = hour_hash.fetch("precipProbability")
+    if precip_prob > precip_prob_threshold
+      @any_precipitation = true 
+    end
+  end
+
+
 
   erb(:umbrella_results)
 end
